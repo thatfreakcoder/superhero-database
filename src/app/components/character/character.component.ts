549f8a75794710e4cf0631548d369b4fd3b846b1
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2020-11-13 04:14:16 +0530

    update character and search page

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -11,22 +11,32 @@ export class CharacterComponent implements OnInit, OnDestroy {
 	private subscriber: any;
 	charId:number = 1;
 	info: any = {};
+	powerstats:object;
+	class:number;
   constructor(private router : ActivatedRoute, private api : ApiRequestService) { }
 
   ngOnInit(): void {
   	this.subscriber = this.router.params.subscribe(params => {
   		console.log(parseInt(params.id));
   		this.charId = parseInt(params.id);
-  		this.getCharacter(this.charId);
-  	})
+  	});
+  	this.getCharacterData(this.charId);
   }
-  getCharacter(id:number){
+  getCharacterData(id:number){
   	const promise = this.api.searchByID(id).toPromise();
   	promise.then((data) => {
   		this.info = data;
   		console.log(this.info);
-  	})
+  		this.powerstats = data['powerstats'];
+	  	console.log(this.powerstats);
+	  	this.getClass(this.powerstats)
+  	});
   }
+  getClass(powerstats:object):void{
+  	this.class = (powerstats['intelligence']**1.3 + (powerstats['strength']*0.5 )**2 + (powerstats['speed']*0.5)**2 + powerstats['durability']^1.6 + (powerstats['power'] + powerstats['combat']**1.8 ))
+  	console.log(this.class);
+  	
+}
   ngOnDestroy(): void{
   	this.subscriber.unsubscribe();
   }
